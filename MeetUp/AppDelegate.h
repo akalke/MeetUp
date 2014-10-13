97dde0e2d39e4e3dc8297c3aafc9c53dc1df12b8
akalke <amaeya.kalke@gmail.com>
2014-10-13 12:05:49 -0500

    Initial Commit

---
+++ MeetUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetUp
+//
+//  Created by Amaeya Kalke on 10/13/14.
+//  Copyright (c) 2014 Amaeya Kalke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
